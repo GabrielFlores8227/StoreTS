a61070abbf8eda2b1d4eeb396cac7750f1bdf8c8
GabrielFlores8227 <jgdaflores184186@hotmail.com>
2023-05-09 15:17:00 -0300

    dev

--- src/router/admin/api/api.ts
+++ src/router/admin/api/api.ts
@@ -4,29 +4,30 @@ import LocalModules from './.localModules';
 const api: express.Router = express.Router();
 
 //*NEW SECTION
-api.post('/propaganda', LocalModules.middlewareUploadFiles(2, 2), LocalModules.middlewareCheckAuth, LocalModules.middlewarePostPropaganda, LocalModules.middlewareSendResponse(200));
+api.post('/propagandas', LocalModules.middlewareUploadFiles(2, 2), LocalModules.middlewareCheckAuth, LocalModules.middlewarePostPropaganda, LocalModules.middlewareSendResponse(200));
 
-api.delete('/propaganda', LocalModules.middlewareCheckAuth, LocalModules.middlewareDeletePropaganda, LocalModules.middlewareSendResponse(200));
-
-//*NEW SECTION
-api.post('/category', LocalModules.middlewareCheckAuth, LocalModules.middlewarePostCategory, LocalModules.middlewareSendResponse(200));
-
-api.delete('/category', LocalModules.middlewareCheckAuth, LocalModules.middlewareDeleteCategory, LocalModules.middlewareSendResponse(200));
+api.delete('/propagandas', LocalModules.middlewareCheckAuth, LocalModules.middlewareDeletePropaganda, LocalModules.middlewareSendResponse(200));
 
 //*NEW SECTION
-api.post('/product', LocalModules.middlewareUploadFiles(1, 1), LocalModules.middlewareCheckAuth, LocalModules.middlewarePostProduct, LocalModules.middlewareSendResponse(200));
+api.post('/categories', LocalModules.middlewareCheckAuth, LocalModules.middlewarePostCategory, LocalModules.middlewareSendResponse(200));
 
-api.delete('/product', LocalModules.middlewareCheckAuth, LocalModules.middlewareDeleteProduct, LocalModules.middlewareSendResponse(200));
+api.delete('/categories', LocalModules.middlewareCheckAuth, LocalModules.middlewareDeleteCategory, LocalModules.middlewareSendResponse(200));
 
 //*NEW SECTION
-api.put('/text', LocalModules.middlewareCheckAuth, LocalModules.middlewarePutText, LocalModules.middlewareSendResponse(200));
+api.post('/products', LocalModules.middlewareUploadFiles(1, 1), LocalModules.middlewareCheckAuth, LocalModules.middlewarePostProduct, LocalModules.middlewareSendResponse(200));
+
+api.delete('/products', LocalModules.middlewareCheckAuth, LocalModules.middlewareDeleteProduct, LocalModules.middlewareSendResponse(200));
+
+//*NEW SECTION
+api.put('/t/:table/:column', LocalModules.middlewareCheckAuth, LocalModules.middlewarePutText, LocalModules.middlewareSendResponse(200));
+
+api.put('/i/:table/:column', LocalModules.middlewareUploadFiles(1, 1), LocalModules.middlewarePutImage, LocalModules.middlewareCheckAuth, LocalModules.middlewareSendResponse(200));
 
 /*
 
 api.put('/login', LocalModules.middlewareCheckAuth, LocalModules.middlewarePutLogin, LocalModules.middlewareSendResponse(200));
 
 
-api.put('/image', LocalModules.middlewareUploadFiles('files', 1), LocalModules.middlewareCheckAuth, LocalModules.middlewarePutImage, LocalModules.middlewareSendResponse(200));
 
 api.put('/position', LocalModules.middlewareCheckAuth, LocalModules.middlewarePutPosition, LocalModules.middlewareSendResponse(200));
 
