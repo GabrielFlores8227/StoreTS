9c3f1f6e7ea38151826eb3a2483f4a328143048f
GabrielFlores8227 <jgdaflores184186@hotmail.com>
2023-05-16 18:25:40 -0300

    dev

--- src/back-end/router/root/api/localModules.ts
+++ src/back-end/router/root/api/localModules.ts
@@ -89,7 +89,7 @@ export default class LocalModules {
 
 			await GlobalSqlModules.sqlQuery(GlobalSqlModules.sqlMasterConn, 'UPDATE products SET clicks = ?, lastClick = CURRENT_TIMESTAMP WHERE id = ?;', [clicks, id!])
 
-			Object(req).redirectTo = "https://api.whatsapp.com/send?phone=" + Object(query)[0].whatsapp + "&text=" + Object(query)[0].message.replace("###", Object(query)[0].name).replace(/ /g, "%")
+			Object(req).redirectTo = "https://api.whatsapp.com/send?phone=" + Object(query)[0].whatsapp + "&text=" + Object(query)[0].message.replace("###", Object(query)[0].name)
 
 			return next()
 		} catch(err) {
