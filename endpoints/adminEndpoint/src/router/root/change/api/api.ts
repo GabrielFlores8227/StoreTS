7e5908db5c9084eacccb6b8b36d4b10b6dfb7a9f
Gabriel Flores <jgdaflores184186@hotmail.com>
2023-06-23 14:14:31 -0300

    fixes #16

--- endpoints/adminEndpoint/src/router/root/change/api/api.ts
+++ endpoints/adminEndpoint/src/router/root/change/api/api.ts
@@ -1,11 +1,23 @@
 import express from 'express';
 import Middleware from 'storets-middleware';
+import { rateLimit } from 'express-rate-limit';
 import LocalModules from './localModules';
 
 const api = express.Router();
 
 api.put(
 	['/username', '/password'],
+	rateLimit({
+		windowMs: 20 * 60 * 1000,
+		max: 30,
+		handler: (_, res) => {
+			res.status(429).json({
+				status: 429,
+				message:
+					'Devido ao número máximo de pedidos ser atingido, a ação de atualizar o login será bloqueada temporariamente por alguns minutos neste dispositivo.',
+			});
+		},
+	}),
 	Middleware.middlewareCheckToken,
 	LocalModules.middlewarePutAuth,
 	(_, res) => {
