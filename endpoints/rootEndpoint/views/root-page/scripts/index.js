5b343e027f4a98fe4c54dfab2b1ec551332a2aaa
Gabriel Flores <jgdaflores184186@hotmail.com>
2023-07-13 12:40:21 -0300

    update

--- endpoints/rootEndpoint/views/root-page/scripts/index.js
+++ endpoints/rootEndpoint/views/root-page/scripts/index.js
@@ -192,7 +192,7 @@ const touchSliderController = [];
 						behavior: 'smooth',
 					});
 				},
-				/iPhone|iPad|iPod|Android/i.test(navigator.userAgent) ? 4000 : 3000,
+				/iPhone|iPad|iPod|Android/i.test(navigator.userAgent) ? 5000 : 4000,
 			);
 		};
 
